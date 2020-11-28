6b31f83e1db6da169067efb56853e9ae14252541
Kevin Roig <kevinroig37@gmail.com>
2020-11-28 19:09:42 +0100

    made a navbar, great colours and a favicon

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -14,7 +14,7 @@ class DefaultController extends AbstractController
     public function index(): Response
     {
         return $this->render('index.html.twig', [
-            'title' => 'Bienvenue!',
+            'title' => 'Bienvenue sur Wild séries!',
         ]);
     }
 }
